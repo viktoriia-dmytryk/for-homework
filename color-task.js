8e9bfd3913b2df6c6a6beed766932c44d9fc1f1c
viktoriia-dmytryk <93657141+viktoriia-dmytryk@users.noreply.github.com>
2026-04-22 09:36:28 +0000

    Deploying to gh-pages from @ viktoriia-dmytryk/for-homework@1764261c6ff5d2d3c3c73fdf5586b3cf84234d5f 🚀

---
+++ color-task.js
@@ -0,0 +1,2 @@
+import{r as e,b as r,g as t,S as o}from"./assets/handlers-FSqBTaty.js";/* empty css                      */e.colorSelect.addEventListener("change",r);document.addEventListener("DOMContentLoaded",a=>{e.colorStatus.textContent=t(o.COLOR_STATUS)||"Обраний колір: blue",e.colorSelect.value=t(o.COLOR_SELECT)||"blue"});
+//# sourceMappingURL=color-task.js.map
---
+++ switch-task.js
@@ -0,0 +1,2 @@
+import{r as t,g as o,a as n,S as e}from"./assets/handlers-FSqBTaty.js";/* empty css                      */document.addEventListener("DOMContentLoaded",()=>{t.notificationsCheckbox.checked=o(e.CHECKBOX_NOTIFICATIONS)||!1,t.notificationsStatus.textContent=o(e.STATUS_NOTIFICATIONS)||"Сповіщення вимкнені"});t.notificationsCheckbox.addEventListener("change",n);
+//# sourceMappingURL=switch-task.js.map
